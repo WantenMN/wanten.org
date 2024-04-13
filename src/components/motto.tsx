d321660a8508910842a7a8a4e05ecdf872b6cbc2
Wanten <WantenMN@gmail.com>
2024-04-14 00:39:49 +0800

    chore: update motto

--- src/components/motto.tsx
+++ src/components/motto.tsx
@@ -3,7 +3,7 @@ const Motto = () => {
     <div className="flex w-full items-center justify-center leading-none">
       <p className="text-sm text-zinc-500">
         <span className="select-none">- </span>
-        <span>Do first, perfect later</span>
+        <span>Do first, refine later</span>
         <span className="select-none"> -</span>
       </p>
     </div>
