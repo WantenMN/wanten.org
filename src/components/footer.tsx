eafa0658347695ae7738892253d3e4a426badc35
Wanten <WantenMN@gmail.com>
2024-04-14 10:14:09 +0800

    style: container, footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import Motto from "./motto";
 
 const Footer = () => {
   return (
-    <footer className="rounded rounded-t-none border-zinc-200 bg-white p-4 sm:m-4 sm:mt-0 sm:border sm:border-t-0 sm:border-zinc-200">
+    <footer>
       <Motto />
     </footer>
   );
