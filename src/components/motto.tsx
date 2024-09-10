a6490d7289172e7e044fc7675d5e0a4688c9daee
Wanten <WantenMN@gmail.com>
2024-09-10 18:17:56 +0800

    feat(motto): update motto text

--- src/components/motto.tsx
+++ src/components/motto.tsx
@@ -1,7 +1,7 @@
 const Motto = () => {
   return (
     <p className="border-t border-zinc-100 pt-4 text-center text-sm italic text-zinc-600">
-      &quot;Do first, refine later&quot;
+      &quot;Do first, fix later&quot;
     </p>
   );
 };
