2f2a0cd44dcc4f7c0f50cfd630eac4b45ab374af
Wanten <WantenMN@gmail.com>
2024-11-12 19:06:36 +0800

    feat: edit history

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,22 +1,34 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+
 import GiscusComments from "@/hooks/GiscusComments";
 import { PostData } from "@/lib/posts";
 
 import { Separator } from "./ui/separator";
 
 const Post = ({ postData }: { postData: PostData }) => {
+  const pathname = usePathname();
   GiscusComments();
 
   return (
     <article className="markdown-body">
       <h1>{postData.title}</h1>
-      <span className="mb-8 mt-4 block text-sm text-zinc-400">
-        {postData.time}
-      </span>
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      <section className="mb-8 mt-4 text-sm">
+        <a
+          className="inline-block !text-zinc-400"
+          title="Edit History"
+          href={`http://github.com/WantenMN/wanten.org/commits/main/posts${pathname}.md`}
+          target="_blank"
+        >
+          <span>{postData.time}</span>
+        </a>
+      </section>
+      <section
+        dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+      ></section>
       <Separator />
-      <div className="giscus"></div>
+      <section className="giscus"></section>
     </article>
   );
 };
