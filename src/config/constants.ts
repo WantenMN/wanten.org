af3adb06753dd44456c1d19115e4dd0aa96b780d
Wanten <WantenMN@gmail.com>
2025-07-11 11:15:21 +0800

    feat: remove thoughts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_TITILE = "Wanten";
 export const BASE_DESC =
   "Wanten's personal website showcases his work and thoughts.";
-export const CATEGORIES = ["thoughts", "works", "notes"] as const;
+export const CATEGORIES = ["works", "notes"] as const;
